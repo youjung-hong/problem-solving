62dd94c672692cb2f7c1daa50813ecb13c37d4e4
youjung-hong <dev.youjung.hong@gmail.com>
2023-03-16 22:37:19 +0900

    코테문제 품

---
+++ Solution.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        // 문자열 별로 
+    }
+
+    // 시간복잡도는 O(n) * O(20) = O(n)
+    // 공간복잡도는 20n
+}
